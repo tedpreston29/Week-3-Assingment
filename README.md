2596cdf5a72d4afd9c06691f0e9f462f9e5298e8
Ted Preston <tedpreston29@gmail.com>
2025-09-18 16:25:45 +0100

    API and for each loop added

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Week-3-Assingment
+
+Added relevant HTML elemtents linked them to JS. (nice and easy)
+
+Added cookieCount and cps and gave them values. (also easy)
+
+Created the setInterval function to add a cookie every one second. Updated the DOM so that the "Cookies Collected" and "CPS" would be displayed on screen.
+Added ability for saving in local storage using localStorage.setItem. Also used localStorage.getItem so if page was refreshed the count would continue unless cleared manually. (Used yesterdays workshop to help me out with all this)
+
+Gave the cookieImage an event listener so that when clicked it would increas the count by plus 1.
--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const cookieDisplay = document.getElementById("cookieDisplay");
 const cpsCounter = document.getElementById("cpsCounter");
 const cookieImage = document.getElementById("cookie");
+const shopUpgrades = document.getElementById("shop");
 
 let state = localStorage.getItem("state");
 if (state) {
@@ -24,3 +25,15 @@ cookieImage.addEventListener("click", function () {
   state.cookieCount++;
   cookieDisplay.textContent = "Cookies Collected: " + state.cookieCount;
 });
+
+async function fetchUpgrades() {
+  const response = await fetch(
+    "https://cookie-upgrade-api.vercel.app/api/upgrades"
+  );
+  const upgrades = await response.json();
+
+  upgrades.forEach(function (upgrade) {
+    console.log(upgrade.id, upgrade.name, upgrade.cost, upgrade.increase);
+  });
+}
+fetchUpgrades();
